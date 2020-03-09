f9382d0335b36a8c2341e4d3e5b50446e587f648
riden1862 <riden1862@7HGM6Y1.Student.UGDSB.ED>
2020-03-09 14:36:59 -0400

    Final Commit Before Submitting

--- src/MultipleChoiceQuizGUI.java
+++ src/MultipleChoiceQuizGUI.java
@@ -2,9 +2,9 @@
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Riley Denoon
+Multiple Choice GUI
+Submitted March 9th 2020
  */
 
 /**
